27b908b52ffc98213ae8f7e2293b7b12a065e7be
EC2 Default User <ec2-user@ip-172-31-16-162.ap-southeast-1.compute.internal>
2019-09-09 23:21:55 +0000

    不要ファイル削除２

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # ユーザーを一人ずつ作成します
-Employee.create!(name: '山田太郎', email: 'exampleInputEmail1.gmail.com', image: 'user_1.png',password: 'foobar')
-Employee.create!(name: '佐藤太郎', email: 'exampleInputEmail2.gmail.com', image: 'user_2.png',password: 'foobar')
-Employee.create!(name: '鈴木太郎', email: 'exampleInputEmail3.gmail.com', image: 'user_3.png',password: 'foobar')
-Employee.create!(name: '大谷太郎', email: 'exampleInputEmail4.gmail.com', image: 'user_4.png',password: 'foobar')
-Employee.create!(name: '田中太郎', email: 'exampleInputEmail5.gmail.com', image: 'user_5.png',password: 'foobar')
+Employee.create!(password: 'foobar')
+Employee.create!(password: 'foobar')
+Employee.create!(password: 'foobar')
+Employee.create!(password: 'foobar')
+Employee.create!(password: 'foobar')
